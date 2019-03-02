5c3b023ee59af1a5781b31b573ef94b7591d1cdb
magdahime <magdalena.majkowska1@gmail.com>
2019-03-02 20:47:02 +0100

    Add Thungry_client.h
    This is a header file for Thungry_client class.

---
+++ Thungry_client.h
@@ -0,0 +1,18 @@
+#ifndef THUNGRY_CLIENT_H_
+#define THUNGRY_CLIENT_H_
+#include<string>
+class Thungry_client{
+public:
+std::string name;
+double money;
+int level_of_hunger{10};
+int level_of_impatience{0};
+int rating{0};
+
+void give_5_stars();
+void order_food(std::string food);
+void get_angry(int price);
+void eat(int taste, int price);
+void go_to_the_restaurant(std::string name1, double money1);
+};
+#endif
